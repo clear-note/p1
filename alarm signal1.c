9bef8d3d66c3986fe95f32943de4287c800a72c3
clearnote <zkszks9741@gmail.com>
2018-10-22 01:54:44 +0900

    Add files via upload

---
+++ alarm signal1.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+#include <signal.h>
+
+//#include <siginfo.h>
+
+#include <stdio.h>
+
+
+
+void handler(int signo) {
+
+        psignal(signo, "Received Signal");
+
+}
+
+
+
+int main(void) {
+
+        sigset(SIGALRM, handler);
+
+
+
+        alarm(2);
+
+        printf("Wait... ");
+
+        sleep(3);
+
+
+
+        return 0;
+
+}
